0600470fc3a531f0582a25774c8d1a89ce6b99cb
Aman Toor <amanst.04s@gmail.com>
2024-03-27 17:45:47 -0400

    fixed

--- SitterFinder.m
+++ SitterFinder.m
@@ -10,25 +10,50 @@ conn = database(datasource, username, password);
 % Extracting columns from cell arrays
 ownerMatching = ownerinfo([4, 5, 6]);
 petMatches = petinfo([2, 4, 5]);
-%Concatenating extracted columns from cell arrays
+% Concatenating extracted columns from cell arrays
 infoCellArray = [ownerMatching, petMatches];
-disp(infoCellArray)
 
-% Execute the SQL query and fetch the data
-sqlQuery = ['SELECT city, province, country, Petbreed, PetBehaviour, PetGender ' ...
+% Update the SQL query to fetch Name and PhoneNumber along with other details
+sqlQuery = ['SELECT name, PhoneNumber, city, province, country, Petbreed, PetBehaviour, PetGender ' ...
     'FROM Sitters'];
 data = fetch(conn, sqlQuery);
-%convert to cellarray%
-rowinfo = data;
-disp(rowinfo)
 
-
-% Check if a match was found
-are_equal_positions = isequal(rowinfo(1,:), infoCellArray);
-
-  if are_equal_positions
-    disp('The specified positions in the cell arrays are equal.');
+% Ensure rowinfo is a cell array, if data is a table
+if istable(data)
+    rowinfo = table2cell(data);
 else
-    disp('The specified positions in the cell arrays are not equal.');
+    rowinfo = data; % Assuming data is already a cell array
 end
+
+% Adjust the comparison logic
+numRowsInfo = size(infoCellArray, 1);
+numRowsRowInfo = size(rowinfo, 1);
+
+% Initialize variables to hold the first match's Name and PhoneNumber
+firstMatchName = '';
+firstMatchPhoneNumber = '';
+
+for i = 1:numRowsInfo
+    for j = 1:numRowsRowInfo
+        % Compare the current row in infoCellArray with the current row in rowinfo (skipping Name and PhoneNumber fields)
+        if isequal(infoCellArray(i, :), rowinfo(j, 3:end)) % Adjusted for additional columns
+            firstMatchName = rowinfo{j, 1}; % Store the Name of the first match
+            firstMatchPhoneNumber = rowinfo{j, 2}; % Store the PhoneNumber of the first match
+            break; % Found the first match, no need to continue
+        end
+    end
+    
+    if ~isempty(firstMatchName) % If a match is found, break from the outer loop as well
+        break;
+    end
+end
+
+% Display the Name and PhoneNumber of the first match, if found
+if ~isempty(firstMatchName)
+    disp(['First Match Name: ', firstMatchName]);
+    disp(['First Match PhoneNumber: ', firstMatchPhoneNumber]);
+else
+    disp('No match found.');
+end
+
 end
